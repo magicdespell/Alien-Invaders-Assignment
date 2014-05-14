76079ea02bdaa4099b9a15348723b193dce95157
Demetris Markitanis <i7707278@bournemouth.ac.uk>
2014-05-14 13:35:44 +0100

    added more variables to define the update the init function of the drawable object

--- Pan_BG.js
+++ Pan_BG.js
@@ -25,11 +25,13 @@ var imageRepository = new function() {
  * that all child objects will inherit, as well as the default
  * functions.
  */
-function Drawable() {
-	this.init = function(x, y) {
+function Drawable() { 
+	this.init = function(x, y, width, height) {
 		// Default variables
 		this.x = x;
 		this.y = y;
+        this.width = width;
+		this.height = height;
 	}
 	this.speed = 0;
 	this.canvasWidth = 0;
